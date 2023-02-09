54226db6bf325eda613e60bd9074721e2b60da8b
Ricardo Paganini <ricardo.paganini@hotmail.com>
2023-02-09 14:30:52 -0300

    Delete parameters.py

--- CotaçãoMoedas/parameters.py
+++
@@ -1,7 +0,0 @@
-# PARÂMETROS DO TWILIO
-
-account_sid = 'AC2e67699a39d83f56e28ece6798ed6234'
-auth_token = '76d259ed0abcea59b9ecd8b9064806e2'
-sender = '+13855263165'
-recipient = '+5511932731001'
-client = Client(account_sid,auth_token)
